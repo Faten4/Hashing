6ee54309e42a431416cad998e47eb1a292ee4e1e
faten <faten.x44@gmail.com>
2022-11-30 13:11:06 +0300

    up

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import { connectDB } from './config/db';
 import authRouter from './routes/auth.route'
 import studentRouter from './routes/student.routes'
-
+import classroomRouter from './routes/classroom.routes'
+import teacherRouter from './routes/teacher.routes'
 const app = express();
 
 connectDB();
@@ -11,8 +12,8 @@ app.use(express.json());
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/studennt', studentRouter)
-//app.use('/api/v1/classroom', authRouter)
-//app.use('/api/v1/teacher', authRouter)
+app.use('/api/v1/classroom', classroomRouter)
+app.use('/api/v1/teacher', teacherRouter)
 
 
 
